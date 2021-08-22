c66797a953f1b560e212962ba9e4a412aa321da9
riku910 <rikubiz55@gmail.com>
2021-08-22 14:44:28 +0900

    テストコードの実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品機能' do
+    context '出品できる時' do
+      it '全ての内容があれば登録できる' do
+        expect(@item).to be_valid
+      end
+
+
+    end
+    context '出品できない時' do
+      it '商品名がなければ登録できない' do 
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it '商品詳細がなければ登録できない' do 
+        @item.detail = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+
+      it '価格が空では登録できない' do 
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'カテゴリー選択が{---}では登録ができない' do 
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
+      it '商品の状態が{---}では登録ができない' do 
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+
+      it '発送元が{---}では登録ができない' do 
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
+      it '配送料の負担が{---}では登録ができない' do 
+        @item.shipping_price_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping price must be other than 1")
+      end
+
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
+      it '画像がないと出品できない' do 
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
 end
