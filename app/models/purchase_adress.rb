7b801fd4131de51cf740623cb0b0835740768d80
riku910 <rikubiz55@gmail.com>
2021-08-27 17:06:16 +0900

    保存処理の記述

--- app/models/purchase_adress.rb
+++ app/models/purchase_adress.rb
@@ -8,4 +8,9 @@ class PurchaseAddress
     validate  :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1 } 
   end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, building_name: building_name, tel: tel, street: street, purchase_id: purchase.id)
+  end
 end
