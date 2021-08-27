089f2f90521df57317a5dd99ca1ec255caf39624
riku910 <rikubiz55@gmail.com>
2021-08-27 15:58:54 +0900

    テーブルの作成

--- db/migrate/20210827064344_create_addresses.rb
+++ db/migrate/20210827064344_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string     :postal_code,    null: false
+      t.integer    :prefecture_id,  null: false
+      t.string     :municipalities, null: false
+      t.string     :building_name,  null: false
+      t.string     :tel,            null: false
+      t.string     :street,         null: false
+      t.references :purchase,       null: false, foreign_key: true
       t.timestamps
     end
   end
