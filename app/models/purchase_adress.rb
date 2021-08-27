ccb55c7c7788ec71ac194b74bbbfbb9abf357cb2
riku910 <rikubiz55@gmail.com>
2021-08-27 16:56:54 +0900

    バリデーションの設置

--- app/models/purchase_adress.rb
+++ app/models/purchase_adress.rb
@@ -1,2 +1,11 @@
 class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :building_name, :tel, :street, :purchase_id
+
+  with_options presence: true do
+    validates :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :tel, :street, :purchase_id
+    validates :tel, numericality: true
+    validate  :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1 } 
+  end
 end
