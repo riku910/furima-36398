2177d798e9caf6dc9867d00c6bb4abe61b4afe1f
riku910 <rikubiz55@gmail.com>
2021-08-29 15:25:06 +0900

    createアクションの内容の定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,18 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipalities, :street, :building_name, :tel).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,7 +3,7 @@ class PurchaseAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :building_name, :tel, :street, :purchase_id
 
   with_options presence: true do
-    validates  :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :tel, :street, :purchase_id
+    validates  :user_id, :item_id, :postal_code, :prefecture_id, :municipalities, :tel, :street
     validates  :tel, numericality: true
     validates  :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates  :prefecture_id, numericality: { other_than: 1 } 
@@ -11,6 +11,6 @@ class PurchaseAddress
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, building_name: building_name, tel: tel, street: street, purchase_id: purchase.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, building_name: building_name, tel: tel, street: street, purchase_id: purchase.id)
   end
 end
