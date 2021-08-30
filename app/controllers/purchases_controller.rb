e8d76db7a81c8714e8c5634854f6d9ac9fbf3964
riku910 <rikubiz55@gmail.com>
2021-08-30 17:43:31 +0900

    環境変数の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -24,7 +24,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay
-    Payjp.api_key = ""
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
         amount: @item.price,
         card: purchase_params[:token],
