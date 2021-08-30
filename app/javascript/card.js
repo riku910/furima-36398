988198b04bd25582e48b77073f7fdba61626ba7d
riku910 <rikubiz55@gmail.com>
2021-08-30 14:46:29 +0900

    クライアントサイドの決済機能の実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,33 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_673f0ce38ebb9685df6ca9e9");
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信")
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("purchase_address_number").removeAttribute("name");
+      document.getElementById("purchase_address_cvc").removeAttribute("name");
+      document.getElementById("purchase_address_exp_month").removeAttribute("name");
+      document.getElementById("purchase_address_exp_year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 
