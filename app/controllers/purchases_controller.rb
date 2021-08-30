d47095bddc83d97019c6f0581257504f9367fe13
riku910 <rikubiz55@gmail.com>
2021-08-30 14:59:16 +0900

    クライアントサイド　修正1

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,6 +7,7 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    binding.pry
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
